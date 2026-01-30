d466fe8ee3f6cc895a190627126e5fde6760362f
boberek <kwojt13@gmail.com>
2026-01-30 23:00:08 +0100

    BB trees (2-3 trees) files but no implementation

---
+++ dictionaries/bb_tree.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Krzysztof Wójtowicz on 30/01/2026.
+//
+
+#include "bb_tree.h"
+
+namespace dictionaries {
+
+// insert to BB Tree
+void BBTree::insert(int v) {
+    // NOT IMPLEMENTED
+}
+} // dictionaries
---
+++ dictionaries/bb_tree.h
@@ -0,0 +1,19 @@
+//
+// Created by Krzysztof Wójtowicz on 30/01/2026.
+//
+
+#ifndef BB_TREE_H
+#define BB_TREE_H
+
+#include "bst.h"
+
+namespace dictionaries {
+
+class BBTree : public BST {
+public:
+    void insert(int v); // NOT IMPLEMENTED
+};
+
+} // dictionaries
+
+#endif BB_TREE_H
